3859b6153162f366ebc468444d9e7916d99ca51a
rupin <rupin.chheda@gmail.com>
2016-10-27 16:51:23 +0530

    LED Ring Demo Updated

--- Lesson Plans/Beginner/LEDRing/LEDRing.ino
+++ Lesson Plans/Beginner/LEDRing/LEDRing.ino
@@ -1,42 +1,187 @@
-/*This Arduino program demonstrates the ring of LED's on the shield to light up one after the other.
- * Learners can change the variable blinkSpeed to see how a large value or a small value affects the timing of the lights going one and off, one after the other.
- * 
+/*
+The LED’s on the ring are connected from Digital Pin 1 to Digital pin 12 of the Arduino.
+First of all we need to include the idiotware library to our sketch.
+We need to initialize this leds in void setup() function of arduino sketch.
+The void loop() runs over and over again, repeatedly till power is removed. 
+We will use the function for example the sweep(1). The “1” means how many repetitions you wish 
+to do for that animation.This value can only be whole numbers, and not with decimal points.
 */
-
-const byte LEDCount = 12; // // the number of leds
-int blinkSpeed=50;// The time in milliseconds the LED's are going to stay on.
-//int potentiometerPin = A2;   // The Blue Potentiometer is connected to Analog pin 2
-//int minSpeed = 500; // The maximimum value of time in milliseconds to be set between two adjacent led's glowing.
-//int maxSpeed = 10;  // The minimum value of time in milliseconds to be set between two adjacent led's glowing.
+const int led_Pin_Count = 13; // // the number of leds (i.e. the length of the array)
+int led_Pins[] = {1,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12}; //an array of pin numbers to which LEDs are attached
 
 void setup()
-{
-for(int i = 1; i <= LEDCount; i++)   // set all LEDs to output
-  {
-    pinMode(i, OUTPUT); //Set Pins on Arduino to be outputs
-  }  
-}
+    {
+     for(int i = 0; i < led_Pin_Count; i++)   // set all LEDs to output
+        {
+          pinMode(led_Pins[i], OUTPUT);
+          digitalWrite(led_Pins[i], LOW);
+        } 
+    }
 
 void loop()
-{       
-  //int userSpeed = potentiometerValue();   //this variable will store the Potentiometer value
-  //map the Potentiometer value to minimum and maximum speed You can read about the map function here
-  //https://www.arduino.cc/en/Reference/Map
+     {
+       sweep(2);       //2 replicates of the sweep sequence
+       circle(2);      //2 replicates of the circle sequence
+       halfCircle(2);  //4 replicates of the halfCircle sequence
+       curtain(1);    
+       fastSlow(1);   
+     }
 
-  // The map function takes an input value, which has a range 0-1023, and calculates its corresponding value between minSpeed and maxSpeed
- // int blinkSpeed = map(userSpeed, 0, 1023, minSpeed, maxSpeed);  
+
+
+void curtain(int Reps)
+    {
+     for(int k=1; k<(Reps+1); k++)
+        {
+          for(int j=1; j<257; j=j+j)
+             {
+               int LEDCounter=1;
+               int LEDSwitch=1;
+               for(int i=1; i>0 && i<14; i=i+LEDCounter)
+                  {
+                    if(LEDSwitch)
+                      {
+                        digitalWrite(led_Pins[i], HIGH);
+                        delay(j);
+                      }
+                    else
+                      {
+                        digitalWrite(led_Pins[i], LOW);
+                        delay(j);
+                      }
+                    if(i>12)
+                      {
+                        LEDCounter*=-1;
+                        LEDSwitch=0;
+                        digitalWrite(13, LOW);
+                        delay(j);
+                      }
+                  }
+             }
+        }
+    }
+
+void fastSlow(int Reps)
+     {
+       for(int k=1; k<(Reps+1); k++)
+          {
+            for(int j=1; j<257; j=j+j)
+               {
+                 int LEDCounter=1;
+                 for(int i=1; i>0 && i<14; i=i+LEDCounter)
+                    {
+                      blink(i, j, 10); //Call the blink method below
+                      if(i>12)
+                        {
+                          LEDCounter*=-1;
+                        }
+                    }
+               }
+          }
+      }
+
+
+void blink(int LEDPin, int LEDOnTime, int LEDOffTime)
+          {
+            digitalWrite(led_Pins[LEDPin], HIGH);
+            delay(LEDOnTime);
+            digitalWrite(led_Pins[LEDPin], LOW);
+            delay(LEDOffTime);
+          }
+ 
+ 
+void sweep(int Reps)
+    {
+      for(int k=1; k<(Reps+1); k++)
+         {
+           for(int i=0; i<=led_Pin_Count; i++)
+              {
+                digitalWrite(led_Pins[i],HIGH);
+                delay(100);
+              }
+           for(int i=0; i<led_Pin_Count; i++)
+              {
+                digitalWrite(led_Pins[i],LOW);
+                delay(100);
+              }  
+         }
+       for(int k=1; k<(Reps+1); k++)
+         {
+           for(int i=12; i>=0; i--)
+              {
+                digitalWrite(led_Pins[i],HIGH);
+                delay(100);
+              }
+           for(int i=12; i>=0; i--)
+              {
+                digitalWrite(led_Pins[i],LOW);
+                delay(100);
+              }  
+         }  
+    }  
   
-  for(int i = 1; i <=LEDCount; i++) 
-  {
-    digitalWrite(i, HIGH); //Turn on the LED
-    delay(blinkSpeed); //Keep the LED turned on for a duration of "blinkSpeed" milliseconds. 
-    digitalWrite(i, LOW);   //Turn of the LED 
-  }
-}
+void circle(int Reps)
+    { 
+      for(int k=1; k<(Reps+1); k++)
+         {
+           for(int i= 0,j=12; i<6,j>=6; i++,j--)
+              {
+                digitalWrite(led_Pins[i],HIGH); 
+                digitalWrite(led_Pins[j],HIGH);
+                delay(100);
+              }
+           for(int i= 12,j=0; i>6,j<=led_Pin_Count; i--,j++)
+              {
+                digitalWrite(led_Pins[i],LOW); 
+                digitalWrite(led_Pins[j],LOW);
+                delay(100);
+              }
+              
+            for(int i= 0,j=12; i<6,j>=6; i++,j--)
+              {
+                digitalWrite(led_Pins[i],HIGH); 
+                digitalWrite(led_Pins[j],HIGH);
+                delay(100);
+              }
+           for(int i= 6,j=6; i>6,j<=led_Pin_Count; i--,j++)
+              {
+                digitalWrite(led_Pins[i],LOW); 
+                digitalWrite(led_Pins[j],LOW);
+                delay(100);
+              }  
+            
+        }    
+    }  
+ 
+ void halfCircle(int Reps)
+    { 
+      for(int k=1; k<(Reps+1); k++)
+         {
+           for(int i=0,j=6; i<=6,j<=12;i++,j++)
+              {
+                digitalWrite(led_Pins[i],HIGH); 
+                digitalWrite(led_Pins[j],HIGH);
+                delay(100);
+              }
+           for(int i=6,j=0; i<=12,j<=6;i++,j++)
+              {
+                digitalWrite(led_Pins[i],LOW); 
+                digitalWrite(led_Pins[j],LOW);
+                delay(100);
+              } 
 
-//function to calculate potentiometer value
-/*int potentiometerValue()
-{
-  int val = analogRead(potentiometerPin); //Read the analog position of the potentiometer
-  return val; //return it
-} */
+           for(int i=12,j=6; i>=6,j>=0; i--,j--)
+              {
+                digitalWrite(led_Pins[i],HIGH); 
+                digitalWrite(led_Pins[j],HIGH);
+                delay(100);
+              }
+           for(int i=12,j=6; i>=6,j>=0;i--,j--)
+              {
+                digitalWrite(led_Pins[i],LOW); 
+                digitalWrite(led_Pins[j],LOW);
+                delay(100);
+              }      
+          } 
+    }    
+        
--- Lesson Plans/Beginner/LEDRing/readme.md
+++ Lesson Plans/Beginner/LEDRing/readme.md
@@ -2,11 +2,43 @@
 
 This Arduino program demonstrates the ring of LED's on the shield to light up one after the other.
 
-Learners can change the variable blinkSpeed to see how a large value or a small value affects the timing of the lights going one and off, one after the other.
-
 [![LED Ring Demo](http://img.youtube.com/vi/NEE72VXsWSE/0.jpg)](https://www.youtube.com/watch?v=NEE72VXsWSE)
 
 #Hardware Required
 1. idIoTware Shield
 2. Arduino Uno.
+3. USB cable to connect arduino  to computer
+4. One jumper to enable the LED Ring
+
+The following patterns are already defined in the idIoTware Arduino library.
+a) Sweep,
+b) Half Circle
+c) A Curtain
+d) Fast and Slow Pattern.
+
+
+The LED’s on the ring are connected from Digital Pin 1 to Digital pin 12 of the Arduino.
+First of all we need to include the idiotware library to our sketch.
+We need to initialize this leds in void setup() function of arduino sketch.
+The void loop() runs over and over again, repeatedly till power is removed. 
+We will use the function for example the sweep(1). 
+The “1” means how many repetitions you wish to do for that animation.
+This value can only be whole numbers, and not with decimal points. 
+
+
+In a similar manner, you can also use the other functions to mix and match animations on the LED Ring..
+
+
+Additionally,you can use touch pad or potentiometer as the input to control the 12 led’s.
+
+
+
+
+You can now make your own program and create patterns on the LED’s and put this up on your Christmas tree
+or any other decorations to bring that interactive look to your project. 
+No more connecting leds to breadboard and messy wiring.
+
+
+
+
 
