baea1af1fc94e00f2225974716040d54006c1858
rupin <rupin.chheda@gmail.com>
2017-07-28 14:39:29 +0530

    Updated Police Siren

--- Lesson-Plans/Beginner/PoliceSiren/PoliceSiren.ino
+++ Lesson-Plans/Beginner/PoliceSiren/PoliceSiren.ino
@@ -1,11 +1,17 @@
 
-// sets up and initialize CGShield
-#include <Adafruit_NeoPixel.h>
-#include <CGShield.h>
-#include <Wire.h>         // Require for I2C communication
-CGShield fs;             // Instanciate CGShield instance
 
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 // piezo speaker connected to pin A1 
+// Which pin on the Arduino is connected to the NeoPixels?
+
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int buzzerPin = A1;
 #define wait 1
 #define time 10
@@ -13,20 +19,22 @@ int freq;
 
 void setup()
      {
-      
+      pixels.begin(); // This initializes the NeoPixel library.
      }
 
 void loop() 
     {
       for(freq = 150; freq < 1800; freq += 1)
          {
-           color(0,0,freq);  
+           
+           pixels.setPixelColor(0, pixels.Color(0,0,freq)); // Blue Color.  
            tone(buzzerPin, freq, time);     // Beep pin, freq, time
            delay(wait);
          }
       for(freq = 1800; freq > 150; freq -= 1) 
          {
-           color(freq,0,0);
+           
+           pixels.setPixelColor(0, pixels.Color(freq,0,0)); // Red Color
            tone(buzzerPin, freq, time);     // Beep pin, freq, time
            delay(wait);
          }
