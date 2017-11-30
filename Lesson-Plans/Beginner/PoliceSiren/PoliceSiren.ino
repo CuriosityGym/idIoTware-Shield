fec719536ea499c5fee2832d97ca041745080755
rupin <rupin.chheda@gmail.com>
2017-11-30 12:03:24 +0530

    added code to show pixels color

--- Lesson-Plans/Beginner/PoliceSiren/PoliceSiren.ino
+++ Lesson-Plans/Beginner/PoliceSiren/PoliceSiren.ino
@@ -2,12 +2,12 @@
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
-// piezo speaker connected to pin A1 
+// piezo speaker connected to pin A1
 // Which pin on the Arduino is connected to the NeoPixels?
 
-#define PIN            6
+#define PIN 6
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      1
@@ -18,25 +18,27 @@ int buzzerPin = A1;
 int freq;
 
 void setup()
-     {
-      pixels.begin(); // This initializes the NeoPixel library.
-     }
+{
+  pixels.begin(); // This initializes the NeoPixel library.
+}
 
-void loop() 
-    {
-      for(freq = 150; freq < 1800; freq += 1)
-         {
-           
-           pixels.setPixelColor(0, pixels.Color(0,0,freq)); // Blue Color.  
-           tone(buzzerPin, freq, time);     // Beep pin, freq, time
-           delay(wait);
-         }
-      for(freq = 1800; freq > 150; freq -= 1) 
-         {
-           
-           pixels.setPixelColor(0, pixels.Color(freq,0,0)); // Red Color
-           tone(buzzerPin, freq, time);     // Beep pin, freq, time
-           delay(wait);
-         }
-    }
+void loop()
+{
+  for (freq = 150; freq < 1800; freq += 1)
+  {
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, freq)); // Blue Color.
+    pixels.show();
+    tone(buzzerPin, freq, time);     // Beep pin, freq, time
+    delay(wait);
+  }
+  for (freq = 1800; freq > 150; freq -= 1)
+  {
+
+    pixels.setPixelColor(0, pixels.Color(freq, 0, 0)); // Red Color
+    pixels.show();
+    tone(buzzerPin, freq, time);     // Beep pin, freq, time
+    delay(wait);
+  }
+}
 
